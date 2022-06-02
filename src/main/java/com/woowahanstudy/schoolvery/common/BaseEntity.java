71d8fdf1eebba0d7ebef2292c0d75778e6835aef
정수빈 <jeongsubin@jeongsubin-ui-MacBookPro.local>
2022-06-02 12:03:49 +0900

    feat: Add BaseEntity

---
+++ src/main/java/com/woowahanstudy/schoolvery/common/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.woowahanstudy.schoolvery.common;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public class BaseEntity {
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
