706e757baf91245a1c3e904cb43026e634b62f19
정수빈 <jeongsubin@jeongsubin-ui-MacBookPro.local>
2022-06-02 12:02:05 +0900

    build: Initialize springboot project

---
+++ src/main/java/com/woowahanstudy/schoolvery/SchoolveryApplication.java
@@ -0,0 +1,14 @@
+package com.woowahanstudy.schoolvery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolveryApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SchoolveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woowahanstudy/schoolvery/SchoolveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowahanstudy.schoolvery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
